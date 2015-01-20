42c553b8477c63ae1d92c759c0e27f85ec844d68
GMotro <motrogena@yahoo.com>
2015-01-19 22:09:57 -0700

    Implemented echo/abc function.
    Modified echo.php to print out each argument to the `abc` function on a
    new line. It works for an arbitrary number of arguments. 'Echo' function
    not works, needs PECL extensions to use 'override_function'.

--- php/echo.php
+++ php/echo.php
@@ -2,9 +2,22 @@
 
 function echo()
 {
-  // ...
+    for($i = 0; $i < func_num_args(); $i++) {       
+		echo '\''. func_get_arg($i). '\''."</br>" ;		
+    }
 }
 
 echo();
 echo('bla');
 echo('foo', 'bar', 'baz');
+
+function abc() {
+	
+    for($i = 0; $i < func_num_args(); $i++) {       
+		echo '\''. func_get_arg($i). '\''."</br>" ;		
+    }
+}
+
+abc();
+abc('bla');
+abc('foo', 'bar', 'baz', 'test');
