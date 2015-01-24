0003d37d09cd057967ccbb1eaa5f477a71166d77
GMotro <motrogena@yahoo.com>
2015-01-24 11:27:48 -0700

    added override_function()
    added override_function() to the commented section

--- php/echo.php
+++ php/echo.php
@@ -1,5 +1,6 @@
 <?php
 /*
+override_function('echo', '', 'for($i = 0; $i < func_num_args(); $i++) {print '\''. func_get_arg($i). '\''."</br>" ;}');
 
 function echo()
 {
@@ -15,10 +16,12 @@ echo('foo', 'bar', 'baz');*/
 function abc() {
 	
     for($i = 0; $i < func_num_args(); $i++) {       
-		echo '\''. func_get_arg($i). '\''."</br>" ;		
+		print '\''. func_get_arg($i). '\''."</br>" ;		
     }
 }
 
 abc();
 abc('bla');
 abc('foo', 'bar', 'baz', 'test');
+
+?>
